6d6edad3db082cab09cd2ff0507493389c2612a8
diazlezdo@gmail.com <mariaantonydias.m@lezdotechmed.com>
2025-01-23 13:30:19 +0530

    sign page completed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -27,11 +27,6 @@ const Footer = () => {
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Licensing
-              </a>
-            </li>
-            <li>
               <a href="#" className="hover:underline">
                 Contact
               </a>
---
+++ app/signup/page.tsx
@@ -0,0 +1,86 @@
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="p-5 border rounded text-gray-500 max-w-lg mx-auto flex flex-col gap-3 mt-10">
+      <form className="max-w-lg flex flex-col gap-3">
+        <div className="col-span-2 sm:col-span-1">
+          <label
+            htmlFor="price"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            First Name
+          </label>
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="..."
+            required
+          />
+        </div>
+        <div className="col-span-2 sm:col-span-1">
+          <label
+            htmlFor="price"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="..."
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="small-input"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="small-input"
+            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="small-input"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="small-input"
+            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <button
+          type="button"
+          className="block text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 mt-2 dark:bg-cyan-500 dark:hover:bg-cyan-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        >
+          Sign up
+        </button>
+      </form>
+      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        Already have an account?{" "}
+        <Link
+          href="/signin"
+          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+        >
+          Login here
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default page;
